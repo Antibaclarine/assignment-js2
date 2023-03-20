0bee8582acd85d78e828a71eec7e4fcc502f9702
clarine <antibaclarine.com>
2023-03-20 07:31:34 +0300

    correction done

--- array.js
+++ array.js
@@ -10,30 +10,30 @@ console.log(pets);
 let arr3=[-5,9,5,3,2,-3,6,8,4,1];
 let numb=arr3.sort((a,b)=>(a-b));
 console.log(numb);
-// let arr= ["apple","mango","apple","orange","mango","mango"];
-//  function removeDuplicates(arr){
+let arr= ["apple","mango","apple","orange","mango","mango"];
+ function removeDuplicates(arr){
     
-//         return [...new Set(arr)];
+        return [...new Set(arr)];
     
-// }
-// console.log(removeDuplicates(arr));
+}
+console.log(removeDuplicates(arr));
     
 
-// let arr5=["the","way","x",4];
-// let wordSearch="the"
-// let wordSearch2="we"
-// if(arr5.includes (wordSearch)){
-// console.log("search")
-// }
-// else
-// {console.log("the search word was not found")}
-// if(arr5.includes(wordSearch2)){
-//         console.log("search")
-// }
-// else{
-//         console.log("the search word not found")
-// }
+let arr5=["the","way","x",4];
+let wordSearch="the"
+let wordSearch2="we"
+if(arr5.includes (wordSearch)){
+console.log("search")
+}
+else
+{console.log("the search word was not found")}
+if(arr5.includes(wordSearch2)){
+        console.log("search")
+}
+else{
+        console.log("the search word not found")
+}
 
 let word="sevink"
 let sorted= word.split('').sort().join('');
-console.log(sorted)
+console.log(sorted);
